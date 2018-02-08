d7a68a1759ba8e1e6135b00a65e3a1797b47d954
Matt Zuzolo <mattzuzolo@outlook.com>
2018-02-08 02:47:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,14 +21,17 @@ function sayHiToGrandma(string) {
   console.log("MY NAME IS MATT")
   
   if (string === string.toLowerCase) {
+    console.log("LOWERCASETEST")
     return "I can't hear you!"
   } 
   
-  else if (string === string.toUpperCase) {
+  else if (string === string.toUpperCase) {\
+    console.log("UPPERCASETEST")
     return "YES INDEED!"
   } 
   
   else if (string === "I love you, Grandma.") {
+    console.log("LOVETEST")
     return "I love you, too."
   }
 }
