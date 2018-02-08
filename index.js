ce35cdd4cf23c54d40c98b5cbf6c1ecb970ccb19
Matt Zuzolo <mattzuzolo@outlook.com>
2018-02-08 02:41:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string) {
     return "lower response"
   } 
   
-  else if (string) {
+  else if (string.toUpperCase === string) {
     string.toUpperCase === string
     return "upper response"
   } 
